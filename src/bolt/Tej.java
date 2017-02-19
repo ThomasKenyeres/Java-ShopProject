c49061af7b2a40be0f0e76d6bbad552595bd6667
ThomasKenyeres <kenyerest97@gmail.com>
2017-02-19 22:02:30 +0100

    Task 1. - Tej class implemented

---
+++ src/bolt/Tej.java
@@ -0,0 +1,52 @@
+package bolt;
+
+import java.util.Date;
+
+public class Tej {
+    private int urtartalom;
+    private String gyarto;
+    private Date szavatossagiIdo;
+    private double zsirtartalom;
+    private long ar;
+
+    public Tej(int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar) {
+        this.urtartalom = urtartalom;
+        this.gyarto = gyarto;
+        this.szavatossagiIdo = szavatossagiIdo;
+        this.zsirtartalom = zsirtartalom;
+        this.ar = ar;
+    }
+
+    public boolean joMeg() {
+        Date date = new Date();
+        if (date.after(szavatossagiIdo)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getUrtartalom() {
+        return urtartalom;
+    }
+
+    public String getGyarto() {
+        return gyarto;
+    }
+
+    public Date getSzavatossagiIdo() {
+        return szavatossagiIdo;
+    }
+
+    public double getZsirtartalom() {
+        return zsirtartalom;
+    }
+
+    public long getAr() {
+        return ar;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}

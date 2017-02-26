7fcdd89a1e6279b4df31ba780dde63e240b96e09
ThomasKenyeres <kenyerest97@gmail.com>
2017-02-26 13:44:47 +0100

    Task 5.2 - added method for many kinds of 'Elelmiszer'

--- src/bolt/BoltFactory.java
+++ src/bolt/BoltFactory.java
@@ -23,12 +23,49 @@ public class BoltFactory {
         return new FeltartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
     }
 
+    public Tej ujFelzsirosTartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo) {
+        return ujTartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, Tej.FELZSIROS);
+    }
+
+    public Tej ujZsirosTartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo) {
+        return ujTartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, Tej.ZSIROS);
+    }
+
+    public Tej ujFelzsirosFeltartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo) {
+        return ujFeltartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, Tej.FELZSIROS);
+    }
+
+    public Tej ujZsirosFeltartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo) {
+        return ujFeltartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, Tej.ZSIROS);
+    }
+
+    public Tej ujFelzsirosLiteresTartosTej(Long vonalKod, String gyarto, Date szavatossagiIdo) {
+        return ujFelzsirosTartosTej(vonalKod, Tej.LITER, gyarto, szavatossagiIdo);
+    }
+
+    public Tej ujZsirosLiteresTartosTej(Long vonalKod, String gyarto, Date szavatossagiIdo) {
+        return ujZsirosTartosTej(vonalKod, Tej.LITER, gyarto, szavatossagiIdo);
+    }
+
+    public Tej ujFelzsirosLiteresFeltartosTej(Long vonalKod, String gyarto, Date szavatossagiIdo) {
+        return ujFelzsirosFeltartosTej(vonalKod, Tej.LITER, gyarto, szavatossagiIdo);
+    }
+
+    public Tej ujZsirosLiteresFeltartosTej(Long vonalKod, String gyarto, Date szavatossagiIdo) {
+        return ujZsirosFeltartosTej(vonalKod, Tej.LITER, gyarto, szavatossagiIdo);
+    }
+
+
     public Sajt ujSajt(Long vonalKod, double suly, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
         return new Sajt(vonalKod, gyarto, szavatossagiIdo);
     }
 
     public Szappan ujSzappan(Long vonalKod, String gyarto, char mosohatas) {
-        return new Szappan();
+        return new Szappan(vonalKod, gyarto, mosohatas);
+    }
+
+    public Szappan ujAMosohatasuSzappan(Long vonalKod, String gyarto) {
+        return ujSzappan(vonalKod, gyarto, 'A');
     }
 
 }
